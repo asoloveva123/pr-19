aa14dddcc2f1126112591fbb5b493a7e795044cf
Настя Соловьева <121259111+asoloveva123@users.noreply.github.com>
2022-12-22 23:54:01 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    public static void Procedure(int n)
+    {
+        string str;
+        for (int i = 1; i <= n; i++)
+        {
+            str = String.Concat(Enumerable.Repeat("m", i));
+            Console.WriteLine("{0}", str);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Количество символов: ");
+        var n = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Procedure(n);
+        Console.ReadKey();
+    }
+}
